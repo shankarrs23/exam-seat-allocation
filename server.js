ca74e5ff4f8f0d85bab4f67b6a23411f5b132ab7
shankarrs23 <shankarrs2307@gmail.com>
2025-05-02 00:52:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 // Database connection
-mongoose.connect('mongodb://localhost:27017/examSeats', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
